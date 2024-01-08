bf3b4decf57142b075f2b5a6cb5ca90f68e30848
talentceffort <47441575+talentceffort@users.noreply.github.com>
2024-01-08 21:19:46 +0900

    [Silver V] Title: 방 번호, Time: 4 ms, Memory: 13208 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.rs
+++ 백준/Silver/1475. 방 번호/방 번호.rs
@@ -2,24 +2,22 @@ use std::io::{self, Read};
 fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
-    let numbers: Vec<u32> = input
+    let numbers: Vec<usize> = input
         .trim()
         .chars()
-        .map(|c| c.to_digit(10).unwrap())
+        .map(|c| c.to_digit(10).unwrap() as usize)
         .collect();
 
     let mut array = vec![0; 10];
 
     for x in numbers {
         if x == 6 || x == 9 {
-            if array[6] > array[9] {
-                array[9] += 1;
-            } else {
-                array[6] += 1;
-            }
+            array[6] += 1;
         } else {
-            array[x as usize] += 1;
+            array[x] += 1;
         }
     }
-    println!("{:?}", array.iter().max().unwrap());
+
+    array[6] = (array[6] + 1) / 2;
+    println!("{}", array.iter().max().unwrap());
 }
