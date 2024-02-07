b6af26b84e78822a7d577992f4c2a120c5e6f071
talentceffort <47441575+talentceffort@users.noreply.github.com>
2024-02-07 22:34:32 +0900

    [Silver III] Title: N과 M (2), Time: 108 ms, Memory: 123316 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,28 @@
+# 자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+
+# 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+
+# (1 ≤ M ≤ N ≤ 8)
+
+from sys import stdin
+
+N, M = map(int, stdin.readline().split())
+
+c = [False for _ in range(10)] # 숫자의 사용 여부를 알기 위한 배열
+
+a = [0 for _ in range(10)] # 결과를 저장 할 배열
+
+def go(index, start, n, m):
+    if index == m:
+        # 수열 출력
+        print(*a[:m])
+        return
+    for i in range(start, n + 1):
+        if c[i]:
+            continue
+        c[i] = True
+        a[index] = i
+        go(index + 1, i + 1, n, m)
+        c[i] = False
+
+go(0, 1, N, M)
