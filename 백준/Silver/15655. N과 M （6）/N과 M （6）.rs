abb96d0d80d9d69a865458cce240478e1bd1111c
talentceffort <47441575+talentceffort@users.noreply.github.com>
2024-02-19 11:23:40 +0900

    [Silver III] Title: N과 M (6), Time: 4 ms, Memory: 13212 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.rs
@@ -0,0 +1,66 @@
+use std::io::{self, Read};
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+    let mut input = input.split_whitespace();
+
+    let n: usize = input.next().unwrap().parse().unwrap();
+    let m: usize = input.next().unwrap().parse().unwrap();
+
+    let mut is_used = vec![false; 10];
+    let mut stack: Vec<usize> = Vec::with_capacity(n);
+    let mut output = String::new();
+    let mut num = vec![0; n];
+    let mut array: Vec<usize> = Vec::with_capacity(n);
+
+    for _ in 0..n {
+        array.push(input.next().unwrap().parse().unwrap());
+    }
+    for i in 0..n {
+        num[i] = array[i];
+    }
+
+    num.sort();
+    solve(0, n, m, &mut is_used, &mut stack, &num, &mut output);
+    println!("{}", output);
+}
+
+fn solve(
+    depth: usize,
+    n: usize,
+    m: usize,
+    is_used: &mut Vec<bool>,
+    stack: &mut Vec<usize>,
+    num: &Vec<usize>,
+    output: &mut String,
+) {
+    if depth == m {
+        writer(output, stack, num);
+        return;
+    }
+
+    let mut start = 0;
+
+    if depth != 0 {
+        start = stack[depth - 1] + 1;
+    }
+
+    for i in start..n {
+        if !is_used[i] {
+            is_used[i] = true;
+            stack.push(i);
+            solve(depth + 1, n, m, is_used, stack, num, output);
+            stack.pop();
+            is_used[i] = false;
+        }
+    }
+}
+
+fn writer(writer: &mut String, stack: &Vec<usize>, num: &Vec<usize>) {
+    for &n in stack.iter() {
+        writer.push_str(&num[n].to_string());
+        writer.push(' ');
+    }
+    writer.push_str("\n");
+}
