196b39beaa82d5770d3463b40f3ea65fd60de8c6
talentceffort <47441575+talentceffort@users.noreply.github.com>
2024-02-20 13:34:40 +0900

    [Silver III] Title: N과 M (7), Time: 368 ms, Memory: 45984 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.rs
@@ -0,0 +1,54 @@
+use std::io::{self, Read};
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+    let mut input = input.split_whitespace();
+
+    let n: usize = input.next().unwrap().parse().unwrap();
+    let m: usize = input.next().unwrap().parse().unwrap();
+
+    let mut stack: Vec<usize> = Vec::with_capacity(n);
+    let mut output = String::new();
+    let mut num = vec![0; n];
+    let mut array: Vec<usize> = Vec::with_capacity(n);
+
+    for _ in 0..n {
+        array.push(input.next().unwrap().parse().unwrap());
+    }
+    for i in 0..n {
+        num[i] = array[i];
+    }
+
+    num.sort();
+    solve(0, n, m, &mut stack, &num, &mut output);
+    println!("{}", output);
+}
+
+fn solve(
+    depth: usize,
+    n: usize,
+    m: usize,
+    stack: &mut Vec<usize>,
+    num: &Vec<usize>,
+    output: &mut String,
+) {
+    if depth == m {
+        writer(output, stack, num);
+        return;
+    }
+
+    for i in 0..n {
+        stack.push(i);
+        solve(depth + 1, n, m, stack, num, output);
+        stack.pop();
+    }
+}
+
+fn writer(writer: &mut String, stack: &Vec<usize>, num: &Vec<usize>) {
+    for &n in stack.iter() {
+        writer.push_str(&num[n].to_string());
+        writer.push(' ');
+    }
+    writer.push_str("\n");
+}
